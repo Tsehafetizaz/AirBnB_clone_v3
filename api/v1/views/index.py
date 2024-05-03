b0a84aba0cf466afae214def73a9b87e1376bc32
Tsehafetizaz <wendetefe@gmail.com>
2024-05-03 11:55:14 -0700

    how small

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python3
-""" Index """
+"""
+API Endpoints Index for AirBnB Clone
+"""
+
+from flask import jsonify
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models import storage
 from api.v1.views import app_views
-from flask import jsonify
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Status of API """
+    """
+    Get the current status of the API.
+    Returns:
+        JSON: A dictionary containing the API status.
+    """
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
-    """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    """
+    Retrieve the count of each object type in the database.
+    Returns:
+        JSON: A dictionary with the count of each object type.
+    """
+    class_map = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
 
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
-
+    num_objs = {name: storage.count(cls) for name, cls in class_map.items()}
     return jsonify(num_objs)
